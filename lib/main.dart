fc9326ef79a8b73589db20a891c991ba9692310d
vinapaulina622 <101787711+vinapaulina622@users.noreply.github.com>
2022-12-01 19:46:00 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void>main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false, 
